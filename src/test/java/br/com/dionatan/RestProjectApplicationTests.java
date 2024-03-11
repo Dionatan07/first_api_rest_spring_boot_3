01eb538d70971d2e1b8763d2066bbaddbcb8b9ad
Dionatan de Andrade <dionatan.andrade@hotmail.com>
2024-03-10 22:10:25 -0300

    My First API Rest with Spring Boot 3

---
+++ src/test/java/br/com/dionatan/RestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dionatan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
